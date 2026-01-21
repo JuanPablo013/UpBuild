4e364e2be5d90dd4ba1af25810b4914e345afe6b
JuanPablo013 <jpbetancourt13@hotmail.com>
2026-01-21 15:03:19 -0500

    Creación del Resource para Users

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Schemas\Schema;
+use Filament\Forms\Components\TextInput;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('email')
+                    ->label('Email address')
+                    ->email()
+                    ->required(),
+                // DateTimePicker::make('email_verified_at'),
+                TextInput::make('password')
+                    ->password()
+                    ->required(),
+            ]);
+    }
+}
